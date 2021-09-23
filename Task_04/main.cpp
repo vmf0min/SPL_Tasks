7ce000288c8bc6f37f571dc3b28932d80c841089
vmf0min <vlifom@yandex.ru>
2021-09-23 17:02:36 +0300

    Task 4.

--- Task_04/main.cpp
+++ Task_04/main.cpp
@@ -11,17 +11,14 @@
  * @copyright Copyright (c) 2021
  */
 
+#include <algorithm>
 #include <iostream>
-#include <limits>
 #include <string>
 
 size_t ReverseNumber(size_t number) {
-  size_t reverse_number = 0;
-  while (number) {
-    reverse_number = reverse_number * 10 + number % 10;
-    number /= 10;
-  }
-  return reverse_number;
+  std::string number_str = std::to_string(number);
+  std::reverse(number_str.begin(), number_str.end());
+  return std::stoull(number_str);
 }
 
 int main() {
